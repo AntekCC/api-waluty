52353d00098669ebec91cd8cdd375724b8a88722
AntekCC <antekch06@gmail.com>
2025-07-23 12:16:00 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,60 +1,53 @@
-﻿using Newtonsoft.Json;
-using System.Runtime.CompilerServices;
-using System.Text;
-string url = "https://api.nbp.pl/api/exchangerates/tables/a/?format=jsonl";
-HttpClient client = new HttpClient();
-var response = await client.GetAsync(url);
-if (response.IsSuccessStatusCode)
-{
-    string json = await response.Content.ReadAsStringAsync();
-    var obj = JsonConvert.DeserializeObject<List<Root>>(json);
-    StringBuilder stringBuilder = new StringBuilder();
-    Dictionary<string, string> map = new Dictionary<string, string>();
-
-    Console.WriteLine("Kody walut");
-    foreach (var root in obj)
-    {
-        foreach (var item in root.rates)
-        {
-            map.Add(item.code, ($"{item.currency} - {item.mid}"));
-            Console.WriteLine($"{item.code} - {item.currency}");
-        }
-
-    }
-    while (true)
-    {
-        Console.WriteLine("podaj kod waluty zeby wyswietlic kurs");
-        try
-        {
-            string input = Console.ReadLine().ToUpper();
-            if (map.ContainsKey(input))
-            {
-                string result = map[input];
-                Console.WriteLine($"Kurs dla {input}: {result}");
-
-            }
-            else
-            {
-                Console.WriteLine("nie znaleziono kodu");
-            }
-        }
-        catch
-        {
-
-        }
-    }
-}
+using Newtonsoft.Json;
 public class Rate
 {
-    public string currency { get; set; }
-    public string code { get; set; }
+    public string no { get; set; }
+    public string effectiveDate { get; set; }
     public double mid { get; set; }
 }
-
 public class Root
 {
     public string table { get; set; }
-    public string no { get; set; }
-    public string effectiveDate { get; set; }
+    public string currency { get; set; }
+    public string code { get; set; }
     public List<Rate> rates { get; set; }
+}
+namespace MyApp
+{
+ class Program
+    {
+        static async Task Main(string[] args)
+        {            
+            while (true)
+            {
+                await currData();
+            }
+        }
+        static async Task currData()
+        {
+            Console.WriteLine("jaka walute chcesz pobrac ?(np USD)");
+            string input = Console.ReadLine().ToUpper();
+            if (!string.IsNullOrEmpty(input) && input.Length == 3)
+            {
+                string url = $"https://api.nbp.pl/api/exchangerates/rates/a/{input}/?format=json";
+                HttpClient client = new HttpClient();
+                var response = await client.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("brak waluty");
+                }
+                else
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    var obj = JsonConvert.DeserializeObject<Root>(json);
+                    foreach (var rate in obj.rates)
+                    {
+                        Console.WriteLine($"{obj.code} - {obj.currency}: {rate.mid}");
+                    }
+
+                }
+
+            }
+        }
+    }
 }
